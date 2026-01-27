f35b0335b7a26c9389fdfba53088953992934670
diwakarkant15 <diwakarkant15@gmail.com>
2026-01-27 18:06:41 +0530

    Added Header section with flowbite

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import About from '../src/pages/About'
+import Dashboard from '../src/pages/Dashboard'
+import Home from '../src/pages/Home'
+import SignIn from '../src/pages/SignIn'
+import SignUp from '../src/pages/SignUp'
+import Projects from '../src/pages/Projects'
+import Headers from './components/Headers'
+import { Button } from 'flowbite-react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Headers />
+      <Routes>
+         <Route path='/' element={<Home />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 

6f3e4de18b40817dde81c4353eeb01ec493c9d5a
diwakarkant15 <diwakarkant15@gmail.com>
2026-01-27 18:21:52 +0530

    Initialized the backend service

---
+++ backend/index.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import 'dotenv/config'
+
+const app = express()
+
+const PORT=process.env.PORT
+
+app.get('/', (req, res)=>{
+    res.send("Hello from backend")
+})
+
+app.listen(PORT, ()=> {
+    console.log(`Server is listening on port ${PORT}`);
+})

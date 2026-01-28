7c161cc20428b00a546b9daef969778f938d291c
diwakarkant15 <diwakarkant15@gmail.com>
2026-01-28 17:37:41 +0530

    completed signin page functionality

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -29,3 +29,37 @@ export const signup = async(req, res, next) => {
   }
 
 }
+
+
+export const signin = async(req, res) => {
+  const {email, password} = req.body;
+  try{
+  const user = await User.findOne({email});
+
+  if(!user){
+    return res.status(401).json({
+      success: "false",
+      message: "Invalid credentials"
+    })
+  }
+
+  const isPasswordCorrect = await bcrypt.compare(password, user.password);
+
+  if(!isPasswordCorrect){
+   return res.status(401).json({message: "Bad request"});
+  }
+
+  return res.status(200).json({
+    success: "true",
+    message: "successfully signed in",
+    user : {
+      id: user._id,
+      email: user.email,
+      name: user.name,
+    }
+  })
+
+}catch(e){
+  return res.status(500).json( {message: "Some error occured", error: e.message});
+}
+}
